bd35e7919d5af318fdb7b3ac8f6b7a2efa75dc63
Sam Courtemanche <sam.courtemanche@epita.fr>
2019-07-12 21:58:56 +0200

    misc: add static root

--- airhack/settings.py
+++ airhack/settings.py
@@ -130,3 +130,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
