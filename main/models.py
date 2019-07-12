73ef788d79b0ad4c8cca1d962ad1e9a6312ada28
Sam Courtemanche <sam.courtemanche@epita.fr>
2019-07-12 22:10:13 +0200

    models: add basic models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Request(models.Model):
+    batch_id = models.CharField(max_length=50)
+    taskers_count = models.IntegerField()
+    tasks_count = models.IntegerField()
+
+
+class Tasker(models.Model):
+    tasker_id = models.IntegerField()
+    request = models.ForeignKey(Request, on_delete=models.CASCADE)
+
+
+class Task(models.Model):
+    task_id = models.CharField(max_length=20)
+    due_time = models.IntegerField()
+    lat = models.FloatField()
+    lng = models.FloatField
+    assignee = models.ForeignKey(Tasker, on_delete=models.CASCADE)
+    request = models.ForeignKey(Request, on_delete=models.CASCADE)
