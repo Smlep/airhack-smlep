876f459ebcc51eb9d2e7f54028d0e3c4da915f08
Sam Courtemanche <sam.courtemanche@epita.fr>
2019-07-13 02:27:56 +0200

    metrics: fix

--- main/metrics.py
+++ main/metrics.py
@@ -3,10 +3,11 @@ from .models import *
 
 def compute_metrics(rq):
     tasks = Task.objects.filter(request=rq)
-    taskers = Tasker.objects.filter(request=rq)
 
-    unassigned = tasks.filter(assignee=None)
+    assigned = tasks.exclude(assignee=None)
 
-    task_percentage = 1 - (len(unassigned) / len(taskers))
+    print(len(assigned))
+    print(len(tasks))
+    task_percentage = len(assigned) / len(tasks)
 
     print('Percentage ' + str(task_percentage))
