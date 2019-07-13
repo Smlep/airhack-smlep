ada7be6c10b3f99495771dc667af7aff3ffe6f6f
Sam Courtemanche <sam.courtemanche@epita.fr>
2019-07-13 02:14:08 +0200

    schedule: come back to all-one-chosse

--- main/views.py
+++ main/views.py
@@ -60,7 +60,7 @@ def entry(request):
         task = Task(due_time=due_time, lat=lat, lng=lng, assignee=None, task_id=task_id, request=rq)
         task.save()
 
-    choose_tasks_closest_rounds(rq)
+    choose_tasks_closest_one_by_one(rq)
     print('Saving new request')
     submit(rq)
     return HttpResponse('Received')
